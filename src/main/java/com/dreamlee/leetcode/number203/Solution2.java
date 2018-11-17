c609935de1b96910205a4b4a2f05cd032d32b7c8
DreamLee <595394202@qq.com>
2018-11-18 01:41:00 +0800

    提交代码

--- src/main/java/com/dreamlee/leetcode/number203/Solution2.java
+++ src/main/java/com/dreamlee/leetcode/number203/Solution2.java
@@ -3,26 +3,23 @@ package com.dreamlee.leetcode.number203;
 /**
  * @author: DreamLee
  * @date: Created on 23:53 2018/11/17
- * @description: 未使用虚拟头节点
+ * @description: 使用虚拟头节点dummyHead
  * @modified:
  */
-public class Solution1 {
+public class Solution2 {
 
     public ListNode removeElements(ListNode head, int val) {
-        while (null != head && head.val == val) {
-            head = head.next;
-        }
-        if (null == head)
-            return null;
-        ListNode prev = head;
-        while (null != prev) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode prev = dummyHead;
+        while (null != prev.next) {
             if (prev.next.val == val) {
                 prev.next = prev.next.next;
             } else {
                 prev = prev.next;
             }
         }
-        return head;
+        return dummyHead.next;
     }
 
 }
